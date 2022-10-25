f83e9260d3491ca573b096ea42a7af2e81fb07e9
Novak1 <jozi258@gmail.com>
2022-10-25 17:26:05 +0200

    init

---
+++ my-meetups/pages/index.js
@@ -0,0 +1,45 @@
+import MeetupList from "../components/meetups/MeetupList";
+import Head from "next/head";
+import { MongoClient } from "mongodb";
+import { Fragment } from "react";
+
+const HomePage = (props) => {
+  return (
+    <Fragment>
+      <Head>
+        <title>React Meetups</title>
+        <meta name='description' content="Browse a huge list of highly active React meetups!"/>
+      </Head>
+      <MeetupList meetups={props.meetups} />;
+    </Fragment>
+  );
+};
+
+
+export async function getStaticProps() {
+
+  const client = await MongoClient.connect(
+    "mongodb+srv://novak:gorstak97@cluster0.ua36ymk.mongodb.net/meetups?retryWrites=true&w=majority"
+  );
+  const db = client.db();
+  const meetupsCollection = db.collection("meetups");
+
+  const meetups = await meetupsCollection.find().toArray();
+
+  client.close();
+
+  return {
+    props: {
+      meetups: meetups.map((meetup) => ({
+        id: meetup._id.toString(),
+        title: meetup.title,
+        address: meetup.address,
+        image: meetup.image,
+      })),
+    },
+  
+    revalidate: 1,
+  };
+}
+
+export default HomePage;
